5da00db8469b910cbfcd1b842e29039617891a5a
George Moschovitis <george-moschovitis@gmosx.com>
2009-03-07 13:14:32 +0200

    Introduced Object.extend as a simple object inheritance helper. Test for Object.extend. String extensions. Misc fixes.

---
+++ examples/blog/src/model/comment.js
@@ -0,0 +1,36 @@
+
+var Comment = exports.Comment = function() {
+
+}
+
+
+/**
+ * "Anonymous" comment
+ */
+var Comment = exports.Comment = function() {
+    
+    this.author = author;
+    this.content = content;
+    
+}
+
+/*
+
+use Atom fields.
+
+Comment
+
+    parentId
+    authorId
+    created
+    content 
+        
+Comment
+
+    parentId
+    authorName
+    authorEmail
+    created
+    content
+
+*/
