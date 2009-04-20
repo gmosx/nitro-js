cd97be0ca40d7ddb9c1e90934c15dd0c901891c1
George Moschovitis <george-moschovitis@gmosx.com>
2009-04-20 14:07:36 +0300

    Introduced a 'json-template' style template.

---
+++ tests/text/template.js
@@ -0,0 +1,20 @@
+var File = require("file").File,
+    Template = require("text/template").Template;
+
+var str = File.read("template.html").toString();
+
+var tmpl = new Template(str);
+
+var data = { 
+    "name": "George",
+    "category": {
+        "label": "Life",
+        "articles": [
+            {"title": "Hello world"},
+            {"title": "Bye world"},
+            {"title": "It works"}
+        ]
+    }
+}
+
+print(tmpl.render(data));
