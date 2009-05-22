a501929837fa268fd885a1d09ecddb5a14d34c78
George Moschovitis <george-moschovits@gmosx.com>
2009-05-22 11:29:05 +0300

    Improved render check.

--- src/nitro/middleware/render.js
+++ src/nitro/middleware/render.js
@@ -30,12 +30,14 @@ exports.Render = function(app, Template, templateRoot) {
         var response = app(env);
 
         // FIXME: better test here.
-        if ((response[0] == 200) && (typeof(response[2]) != "string") && (response[1]["Transfer-Encoding"] != "chunked")) {
+//      if ((response[0] == 200) && (typeof(response[2]) != "string") && (response[1]["Transfer-Encoding"] != "chunked")) {
+        if ((typeof(response[2]) == "object") && (!Array.isArray(response[2]))) { 
             var template = loadTemplate(templateRoot + env["PATH_INFO"]);
             if (template) {
                 response[2] = template.render(response[2]);
             } else {
-            	response[2] = "Template '" + env["PATH_INFO"] + "'not found";
+            	// FIXME, warn: "Template '" + env["PATH_INFO"] + "'not found";
+            	response[2] = "";
             }
         }
 
@@ -43,4 +45,3 @@ exports.Render = function(app, Template, templateRoot) {
     }
     
 }
-
