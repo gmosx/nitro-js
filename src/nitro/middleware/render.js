d5a880f9359f0269f604a7acb6ff26fed65c3a1d
George Moschovitis <george-moschovits@gmosx.com>
2009-06-02 15:04:52 +0300

    Fix for streaming response.

--- src/nitro/middleware/render.js
+++ src/nitro/middleware/render.js
@@ -30,14 +30,12 @@ exports.Render = function(app, Template, templateRoot) {
         var response = app(env);
 
         // FIXME: better test here.
-//      if ((response[0] == 200) && (typeof(response[2]) != "string") && (response[1]["Transfer-Encoding"] != "chunked")) {
-        if ((typeof(response[2]) == "object") && (!Array.isArray(response[2]))) { 
+        if ((typeof(response[2]) == "object") && (!Array.isArray(response[2])) && (response[1]["Transfer-Encoding"] != "chunked")) { 
             var template = loadTemplate(templateRoot + env["PATH_INFO"]);
             if (template) {
                 response[2] = template.render(response[2]);
             } else {
-            	// FIXME, warn: "Template '" + env["PATH_INFO"] + "'not found";
-            	response[2] = "";
+            	response[2] = []; // FIXME, warn template not found?
             }
         }
 
