37ba402e8e770c56fb9891deab2d7ed83c527e20
George Moschovitis <george-moschovitis@gmosx.com>
2009-06-06 15:55:33 +0300

    Fix to make compatible with latest Jack.

--- src/nitro/middleware/render.js
+++ src/nitro/middleware/render.js
@@ -33,7 +33,7 @@ exports.Render = function(app, Template, templateRoot) {
         if ((typeof(response[2]) == "object") && (!Array.isArray(response[2])) && (response[1]["Transfer-Encoding"] != "chunked")) { 
             var template = loadTemplate(templateRoot + env["PATH_INFO"]);
             if (template) {
-                response[2] = template.render(response[2]);
+                response[2] = [template.render(response[2])];
             } else {
             	response[2] = []; // FIXME, warn template not found?
             }
