c0aaaec57e65c3479f85a0b99bcfe78f8f1ccbe7
George Moschovitis <george-moschovitis@gmosx.com>
2009-06-08 11:03:49 +0300

    Moved gae helpers in nitro/etc/gae For extra convenience the origin skel dir is included in etc/gae as well. Manual creation in Eclipse is no longer needed.

---
+++ etc/gae/skel/src/org/jackjs/JackServlet.java
@@ -0,0 +1,13 @@
+package org.jackjs;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JackServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
